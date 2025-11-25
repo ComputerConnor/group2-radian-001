cff06075c950e17c3422785ed27c8de26b9ea57b
PatrickGalloway <pagalloway42@tntech.edu>
2025-11-24 19:08:31 -0600

    Created document

---
+++ greedyKadane.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+if __name__ = "__main__":
+    # Iterate once, tracking best subarray ending at each position
+
+# Example code from Radian's guide:
+#def max_subarray(arr):
+#    max_sum = curr_sum = arr[0]
+#    for x in arr[1:]:
+#        curr_sum = max(x, curr_sum + x)
+#        max_sum = max(max_sum, curr_sum)
+#    return max_sum
